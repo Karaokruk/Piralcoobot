9a572735e510ec0283b7da38aef058874baa78cd
Rpoxane <roxane.flores@etu.u-bordeaux.fr>
2020-12-28 23:59:12 +0100

    ajout des règles

--- app/src/main/java/com/kruk/piralcoobot/rules/PartageTonBreuvageRule.java
+++ app/src/main/java/com/kruk/piralcoobot/rules/PartageTonBreuvageRule.java
@@ -4,7 +4,7 @@ public class PartageTonBreuvageRule extends Rule{
 
     public PartageTonBreuvageRule() {
         this.name = "PartageTonBreuvage";
-        this.ruleText = "<NAME1> verse un peu de la boisson la plus proche dans le verre de <NAME2> (les verres comptent comme une boisson)!";
+        this.ruleText = "<NAME1> verse un peu de la boisson la plus proche dans le verre de <NAME2> (les verres comptent comme une boisson) s";
         this.helpText = "blblbl";
         this.ruleType = ruleTypes.GAME;
     }
@@ -13,7 +13,7 @@ public class PartageTonBreuvageRule extends Rule{
     public String getRuleText(String playerName1, String playerName2) {
         String rule = this.ruleText;
         rule = rule.replaceAll("<NAME1>", playerName1);
-        rule = rule.replaceAll("<NAME2", playerName2);
+        rule = rule.replaceAll("<NAME2>", playerName2);
 
         return rule;
     }
