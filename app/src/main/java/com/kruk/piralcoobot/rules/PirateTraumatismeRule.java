1518b701949481a236359ed85f9c8248487f91ab
Rpoxane <roxane.flores@etu.u-bordeaux.fr>
2020-12-29 00:01:35 +0100

    ajout pirate traumatisme

--- app/src/main/java/com/kruk/piralcoobot/rules/PirateTraumatismeRule.java
+++ app/src/main/java/com/kruk/piralcoobot/rules/PirateTraumatismeRule.java
@@ -1,9 +1,9 @@
 package com.kruk.piralcoobot.rules;
 
-public class PirateRemenberRule extends Rule{
+public class PirateTraumatismeRule extends Rule{
 
-    public PirateRemenberRule() {
-        this.name = "PirateRemenber ";
+    public PirateTraumatismeRule() {
+        this.name = "PirateTraumatisme ";
         this.ruleText = "<PIRATE> bois autant de gorgées que de tentatives que tu as passées au Capitaine Paf";
         this.helpText = "blblbl";
         this.ruleType = ruleTypes.DRINK;
