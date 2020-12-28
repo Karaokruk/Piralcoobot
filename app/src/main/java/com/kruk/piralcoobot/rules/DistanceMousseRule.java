974dbb2884a6ff4375237bedf589599dcf0e4577
Rpoxane <roxane.flores@etu.u-bordeaux.fr>
2020-12-28 22:44:32 +0100

    distance mousse

---
+++ app/src/main/java/com/kruk/piralcoobot/rules/DistanceMousseRule.java
@@ -0,0 +1,16 @@
+package com.kruk.piralcoobot.rules;
+
+public class DistanceMousseRule extends Rule{
+
+    public DistanceMousseRule(){
+        this.name = "dsitanceMousse";
+        this.ruleText = "<NAME> , tu bois autant de gorgées qu'il y a de personnes entre toi et le premier mousse à ta droite !\n Si tu es un mousse bois 5.";
+        this.helpText = "blblbl";
+        this.ruleType = ruleTypes.DRINK;
+    }
+
+    @Override
+    public String getRuleText(String playerName) {
+        return this.ruleText.replaceAll("<NAME>", playerName);
+    }
+}
