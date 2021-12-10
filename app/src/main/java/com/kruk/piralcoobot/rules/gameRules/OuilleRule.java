78af6d66b7a0f36e0938f1720fc2ed846d1b3a7a
Adrien Célérier <karaokruk@hotmail.fr>
2021-12-10 14:41:18 +0100

    Added two rules

---
+++ app/src/main/java/com/kruk/piralcoobot/rules/gameRules/OuilleRule.java
@@ -0,0 +1,26 @@
+package com.kruk.piralcoobot.rules.gameRules;
+
+import com.kruk.piralcoobot.PlayerType;
+import com.kruk.piralcoobot.rules.Rule;
+import com.kruk.piralcoobot.rules.RuleType;
+
+public class OuilleRule extends GameRule {
+
+    public OuilleRule() {
+        super(RuleType.GAME);
+        this.name = "ouille ";
+        this.ruleText = "Chacun son tour, une rime en -ouille ! <NAME> commence, sens vers la <DIRECTION>. PRÊÊÊÊTS ?!\nC'est moiiiiii le plus grand pirate de l'îîîîîîîîle !\nJe trucide et j'écrabouille, et je m'en mets plein les -";
+        this.helpText = "blblbl ;)";
+        this.nbPlayers = 1;
+        this.hasGulps = false;
+        this.playerTypes.add(PlayerType.ANY);
+    }
+
+    @Override
+    public String getRuleText(String playerName) {
+        String ruleText = this.ruleText;
+        ruleText = ruleText.replaceAll("<NAME>", playerName);
+        ruleText = ruleText.replaceAll("<DIRECTION>", getRandomDirection());
+        return ruleText;
+    }
+}
---
+++ app/src/main/java/com/kruk/piralcoobot/rules/roleRules/ClownRule.java
@@ -0,0 +1,22 @@
+package com.kruk.piralcoobot.rules.roleRules;
+
+import com.kruk.piralcoobot.PlayerType;
+import com.kruk.piralcoobot.rules.RuleType;
+
+public class ClownRule extends RoleRule {
+
+    public ClownRule() {
+        super(RuleType.ROLE);
+        this.name = "clown";
+        this.ruleText = "<NAME>, à partir de maintenant, si tu fais rire quelqu'un, la victime prend un cul sec.";
+        this.helpText = "MOI J'SUIS UN CLOWN ?.";
+        this.nbPlayers = 1;
+        this.hasGulps = false;
+        this.playerTypes.add(PlayerType.ANY);
+    }
+
+    @Override
+    public String getRuleText(String playerName) {
+        return this.ruleText.replaceAll("<NAME>", playerName);
+    }
+}
